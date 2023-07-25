07a52934006a0fe81af3366aa5925962784a1dad
MasterYiTheSamurai <blasko.gergo.hun@gmail.com>
2023-07-25 18:19:00 +0200

    Removing comments.

--- main.cpp
+++ main.cpp
@@ -17,63 +17,8 @@ int main()
 
     int index = 0;
 
-    // HEURISTIC CHECK
-    /*Node n(1, 1, 3);
-    Node n1(2, 2, 3);
-    Node n2(3, 3, 3);
-    Node n3(9, 1, 2);
-    Node n4(4, 2, 2);
-    Node n5(6, 3, 2);
-    Node n6(7, 1, 1);
-    Node n7(5, 2, 1);
-    Node n8(8, 3, 1);*/
-
-    // BATGIRL
-    /*Node n(2, 1, 3);
-    Node n1(6, 2, 3);
-    Node n2(3, 3, 3);
-    Node n3(8, 1, 2);
-    Node n4(1, 2, 2);
-    Node n5(5, 3, 2);
-    Node n6(4, 1, 1);
-    Node n7(9, 2, 1);
-    Node n8(7, 3, 1);*/
-
-    // EVOSOFT
-    /* Node n(8, 1, 3);
-     Node n1(9, 2, 3);
-     Node n2(5, 3, 3);
-     Node n3(2, 1, 2);
-     Node n4(3, 2, 2);
-     Node n5(7, 3, 2);
-     Node n6(4, 1, 1);
-     Node n7(1, 2, 1);
-     Node n8(6, 3, 1); */
-
-    // HOW-TO-SOLVE
-    /*Node n(1, 1, 3);
-    Node n1(5, 2, 3);
-    Node n2(2, 3, 3);
-    Node n3(9, 1, 2);
-    Node n4(3, 2, 2);
-    Node n5(6, 3, 2);
-    Node n6(7, 1, 1);
-    Node n7(8, 2, 1);
-    Node n8(4, 3, 1);*/
-
-    /*unique.push_back(n);
-    unique.push_back(n1);
-    unique.push_back(n2);
-    unique.push_back(n3);
-    unique.push_back(n4);
-    unique.push_back(n5);
-    unique.push_back(n6);
-    unique.push_back(n7);
-    unique.push_back(n8);*/
-
     int offset = 9;
 
-    // RANDOM CONFIGURATION
     while (random_numbers.size() != 9)
     {
         tmp = 1 + rand() % 9;
@@ -89,17 +34,21 @@ int main()
         }
     }
 
-    //Display(unique, offset);
-
     Calculate_Manhattan_distance(unique, offset);
 
     Node::Set_f(Node::Get_h());
 
-    prev_turns = Solve(unique, "", prev_turns, offset);
-
+    try
+    {
+        prev_turns = Solve(unique, "", prev_turns, offset);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << "No solution found." << '\n';
+    }
+    
     if (!prev_turns.empty())
     {
-        //std::cout << "DEBUG Ended." << std::endl;
 
         int ind = GetNode(unique, offset);
 
